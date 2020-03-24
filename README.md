d9ded3eb666dc86ad7065e3fa89c355bbb2e630e
Marina Gordynets <timary99@gmail.com>
2020-03-24 15:25:48 +0300

    feat: add html-scraping

---
+++ README.md
@@ -0,0 +1,6 @@
+## Launching:
+
+1. Install all dependencies via `npm i` from `web-scpaping` folder
+2. Type valid variables in `.env` file (you can use `.env-example`)
+2. Run `npm run scraping` in terminal
+3. Enjoy!
---
+++ src/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+(async() => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(process.env.URL);
+
+    // Type into search box.
+    await page.type('.form-control', process.env.EMAIL);
+    await page.type('.form-control.input-last', process.env.PASSWORD);
+    await page.click('button');
+
+
+    // Wait for the results page to load and display the results.
+    const resultsSelector = 'tr[data-date_start|="2020"]';
+    await page.waitForSelector(resultsSelector);
+
+    // Extract the results from the page.
+    const audits = await page.evaluate(resultsSelector => {
+        const names = Array.from(document.querySelectorAll(resultsSelector));
+        return names.map(name => {
+            return name.innerText;
+        });
+    }, resultsSelector);
+    console.log(audits);
+
+    await browser.close();
+})();
